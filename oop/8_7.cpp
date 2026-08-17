99d26d657259768b4cc73986c9581be3bc8b0d55
lolxdop <122689943+lolxdop@users.noreply.github.com>
2026-08-17 12:43:07 +0530

    Add customer records class with input and display methods

---
+++ oop/8_7.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class customer_records{
+    string name;
+    int salary;
+    public:
+    void accept(){
+        cout<<"Enter your name: ";
+        cin>>name;
+        cout<<"Enter your salary: ";
+        cin>>salary;}
+    void display(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"Salary: "<<salary<<endl;}};
+int main() {
+    customer_records c[5];
+    for (int i=0;i<5;i++){
+        c[i].accept();
+    }
+    for (int j=0;j<5;j++){
+        cout<<"Customer "<<j+1<<" Record: "<<endl;
+        c[j].display();
+    }
+    return 0;
+}
