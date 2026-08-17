9b669eeb7a57f436003c5cd1ac3f9ead7cf6b98e
lolxdop <122689943+lolxdop@users.noreply.github.com>
2026-08-17 12:50:09 +0530

    Implement Employee class with accept and display methods

---
+++ oop/8_8.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class Employee{
+    string name, department;
+    int id;
+    public:
+    void accept(){
+        cout<<"Enter your Name: ";
+        cin>>name;
+        cout<<"Enter your ID: ";
+        cin>>id;
+        cout<<"Enter your Department: ";
+        cin>>department;}
+    void display(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"ID: "<<id<<endl;
+        cout<<"Department: "<<department<<endl;}};
+int main() {
+    Employee e[5];
+    for (int i=0;i<5;i++){
+        e[i].accept();
+    }
+    for (int j=0;j<5;j++){
+        cout<<"Emplyee "<<j+1<<" Record: "<<endl;
+        e[j].display();
+    }
+    return 0;
+}
